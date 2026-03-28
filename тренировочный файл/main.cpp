659c8df195d32b267cf02d061b8ff8460237bd20
Evgeniy1003 <nasonov.1003>
2026-03-28 15:52:51 +0300

    Добавлен тренировочный проект

---
+++ тренировочный файл/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <memory>
+
+void PrintInfo()
+{
+	std::unique_ptr<int>ret = std::make_unique<int>(213);
+	std::cout << "    Значение сырого указателя toys=" << *ret << "\n";
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	std::cout << "=== Программа 'toys' начала свою работу! ===" << "\n";
+	PrintInfo();
+	std::cout << "=== Программа 'toys' закончила свою работу! ===" << "\n";
+}
